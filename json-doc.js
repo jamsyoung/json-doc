b45786c321f562a0ab554c4cbd8044358ddd988f
James Young <james.young@turner.com>
2013-08-24 10:35:12 -0400

    argument processing

---
+++ json-doc.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs'),
+    argv,
+    path = require('path'),
+    colors = require('colors'),
+    schema,
+    source,
+    metadata = require('./package.json'),
+    optimist = require('optimist');
+
+argv = optimist
+    .usage('Usage:\n  json-doc source schema\n  json-doc [-h|--help]\n  json-doc [-v|--version]')
+    .alias('help', 'h')
+    .alias('version', 'v')
+    .describe('help', 'You are looking at it.')
+    .describe('version', 'Show the current version installed of dust-compiler')
+    .argv;
+
+if (argv.v) {
+    console.log('json-doc v' + metadata.version);
+    process.exit();
+}
+console.log(JSON.stringify(argv));
+console.log(argv._[0]);
+console.log(argv._[1]);
+if (argv.h || typeof argv._[1] === 'undefined') {
+    optimist.showHelp(function () {
+        optimist.showHelp();
+        console.log('  source         The source JSON file');
+        console.log('  schema         The JSON schema documentation file');
+    });
+    process.exit();
+}
