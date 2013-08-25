372cf0842923fc95741c92c91284f07e8f68da50
James Young <jmeyoung@gmail.com>
2013-08-25 15:48:02 -0400

    Added a unit test!

--- test/mocha/cli-wrapper.test.js
+++ test/mocha/cli-wrapper.test.js
@@ -1,8 +1,17 @@
 /* global describe, it */
 'use strict';
 
+var fs = require('fs'),
+    path = require('path'),
+    childProcess = require('child_process'),
+    jsonDocPath = path.join(path.dirname(fs.realpathSync(__filename)), '../../json-doc-cli.js');
+
+
 describe('Command Line Wrapper', function () {
-    it('sanity check', function () {
-        'string'.should.be.a('string');
+    it('-v should return the current version', function (done) {
+        childProcess.exec(jsonDocPath + ' -v', function (error, stdout, stderr) {
+            stdout.should.equal('json-doc v0.1.0\n');
+            done();
+        });
     });
 });
